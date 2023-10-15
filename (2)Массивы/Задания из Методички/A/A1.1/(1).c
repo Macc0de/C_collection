f60bd22881f6de4b3a9c4d2a69bb849141d60efc
Macc0de <mg3595293@gmail.com>
2023-10-15 15:52:23 +0300

    t

---
+++ (2)Массивы/Задания из Методички/A/A1.1/(1).c
@@ -0,0 +1,58 @@
+// 1) Между первым четным элементом последовательности и последним элементом, значение которого равно квадрату его порядкового номера
+// 1 2 3 4 1 - sum:8, count:3
+#include <stdio.h>
+#include <math.h>
+#define N 5
+
+int func(int A[N])
+{
+	int i, index_chet = 0;
+	for (i = 0; i < N; i++)
+	{
+		if (A[i] % 2 == 0)
+		{
+			index_chet = i;
+			break;
+		}
+	}
+	
+	int index_last = 0;
+	int flag = 0;
+	for (i = N; i > index_chet; i--) // С конца
+	{
+		if (A[i] == pow(i, 2)) // Квадрат порядкового номера
+		{
+			index_last = i;
+			flag = 1;
+			break;
+		}
+	}
+	
+	if (!flag) // Если не нашли, то считаем до конца массива
+		index_last = N;
+	
+	int result = 0, count = 0;
+	for (i = index_chet + 1; i < index_last; i++) 
+	{
+		result += A[i]; // Sum
+		count++;
+	}
+	
+	printf("\nSum: %d, Count: %d", result, count);
+	// Кол-во (index_last-index_chet)
+	return 0;
+}
+
+int main() 
+{
+	int i;
+	int A[N] = {};
+	for (i = 0; i < N; i++) 
+	{
+		scanf("%d", &A[i]);
+	}
+	
+	func(A);
+	
+	return 0;
+}
