4ee9043e5c225c0962ca1f1f50f99babbfe33cbe
Macc0de <mg3595293@gmail.com>
2023-10-16 20:05:12 +0300

    t

---
+++ (2)Массивы/Задания из Методички/B/B1.3/(1).c
@@ -0,0 +1,62 @@
+// Из рисунка (1) 
+#include <stdio.h>
+#include <math.h>
+#define N 100
+
+int is_pow(int num, int stepen)
+{
+	int pow = 1;
+	for (int i = 1; i <= stepen; i++)
+		pow *= num;
+	
+	return pow;
+}
+
+int factorial(int number)    
+{    
+	int i, fact = 1;         
+	for(i = 1; i <= number; i++)
+	{    
+		fact = fact * i;    
+	}      
+	
+	return fact;  
+}   
+
+int main()
+{
+	float x = 0.0, e = 0.0;
+	float summa = 0.0, temp = 0.0, posledniy = 0.0;
+	int sign = 1;
+	
+	printf("x: ");
+	scanf("%f", &x); // Ввод: 2
+	printf("e: ");
+	scanf("%f", &e); // Ввод: 4
+	
+	if (e < 0)
+	{
+		printf("Incorrect input!");
+		return 1;
+	}
+	
+	float function = 0.0;
+	function = exp(-x);
+	
+	for (int i = 0; i < e; i++, sign*=(-1)) // temp < e - возможно
+	{
+		temp = ((float)abs(is_pow(x, i)) / (float)abs(factorial(i))) * sign;
+		summa += temp;
+		posledniy = temp;
+		printf("%f", temp); // Delete
+	}
+	
+	// Основной вывод
+	printf("\n\nx: %.2f, e: %.2f", x, e);
+	printf("\nSumma ryada: %.2lf", summa);
+	printf("\nFunction f(x): %f", function);
+	printf("\nPosledniy chlen ryada: %.2f", fabs(posledniy));
+	printf("\nRaznost': %.2f\n", summa - function);
+
+	return 0;
+}
